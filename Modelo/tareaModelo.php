fd485973cdd3d2c00ad2356dc55f756fd4bbd7ee
Mario <2023110584@udh.edu.pe>
2025-11-18 13:41:47 -0500

    Actualizar tareaModelo.php

--- Modelo/tareaModelo.php
+++ Modelo/tareaModelo.php
@@ -1,18 +1,14 @@
 <?php
 class TareaModelo {
-
     public function obtenerTareas() {
         return []; 
     }
-
     public function insertarTarea($titulo, $categoria) {
         return true;
     }
-
     public function marcarCompletada($id) {
         return true;
     }
-
     public function borrarTarea($id) {
         return true;
     }
