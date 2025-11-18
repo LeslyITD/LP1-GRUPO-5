5336a2f8cf7cf9e85cf0f6bb9d3504655582a825
Mario <2023110584@udh.edu.pe>
2025-11-18 12:48:34 -0500

    Agregar la clase TareaModelo con métodos CRUD

--- Modelo/tareaModelo.php
+++ Modelo/tareaModelo.php
@@ -1,1 +1,21 @@
+<?php
+class TareaModelo {
+
+    public function obtenerTareas() {
+        return []; 
+    }
+
+    public function insertarTarea($titulo, $categoria) {
+        return true;
+    }
+
+    public function marcarCompletada($id) {
+        return true;
+    }
+
+    public function borrarTarea($id) {
+        return true;
+    }
+}
+?>
 
