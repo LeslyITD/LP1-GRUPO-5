63a5c36be68efdf73d4f83e44acf91cda0d75432
Emanuel <2022110529@udh.edu.pe>
2025-12-05 16:23:15 -0500

    Agregar conexion.php y database.txt (Emanuel)

--- Modelo/conexion.php
+++ Modelo/conexion.php
@@ -1,8 +1,24 @@
 <?php
-class Conexion {
+
+class Conn {
+
+    private $dsn;
+    private $usuario;
+    private $pass;
+    private $conexion;
+
+    public function __construct() {
+        $this->dsn = "mysql:host=localhost;dbname=tarea_lp";
+        $this->usuario = "root";
+        $this->pass = "";
+    }
+
     public function conectar() {
-        return "conexión simulada"; 
+        $this->conexion = new PDO($this->dsn, $this->usuario, $this->pass);
+        return $this->conexion;
+    }
+
+    public function cerrar() {
+        $this->conexion = NULL;
     }
 }
-?>
-
