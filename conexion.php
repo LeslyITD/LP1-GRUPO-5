0e7589fab65df5a3d82e993b7f7c0b58d1633aee
LeslyITD <2022210245@udh.edu.pe>
2025-12-05 19:04:25 -0500

    Base de datos y conexion
    se creo la base de datos asi mismo se adjuntó con la conexion

---
+++ conexion.php
@@ -0,0 +1,24 @@
+<?php
+
+class Conn {
+
+    private $dsn;
+    private $usuario;
+    private $pass;
+    private $conexion;
+
+    public function __construct() {
+        $this->dsn = "mysql:host=localhost;dbname=tarea_lp";
+        $this->usuario = "root";
+        $this->pass = "";
+    }
+
+    public function conectar() {
+        $this->conexion = new PDO($this->dsn, $this->usuario, $this->pass);
+        return $this->conexion;
+    }
+
+    public function cerrar() {
+        $this->conexion = NULL;
+    }
+}
